a6b9c2f91f3279f3a4ad013d6a10891b54395fb4
gentle3f <gentle3f@gmail.com>
2026-09-18 02:14:12 +0800

    Add deep music genre style and artist interest families

---
+++ mobile/lib/core/interest_catalog_part9.dart
@@ -0,0 +1,815 @@
+import 'interest_catalog_parser.dart';
+import 'models.dart';
+
+final List<InterestDefinition> kInterestCatalogPart9 = List.unmodifiable([
+  ...parseInterestFamily(
+    idPrefix: 'music.style',
+    category: 'music',
+    cluster: 'music/styles',
+    rankStart: 3100,
+    raw: r'''
+Alternative Rock
+Indie Rock
+Indie Pop
+Dream Pop
+Shoegaze
+Britpop
+Grunge
+Post-Grunge
+Post-Rock
+Progressive Rock
+Art Rock
+Psychedelic Rock
+Garage Rock
+Hard Rock
+Classic Rock
+Southern Rock
+Glam Rock
+Arena Rock
+Math Rock
+Noise Rock
+Space Rock
+Surf Rock
+Rockabilly
+Punk Rock
+Hardcore Punk
+Pop Punk
+Post-Punk
+New Wave
+Emo
+Emo Pop
+Screamo
+Goth Rock
+Industrial Rock
+Metalcore
+Deathcore
+Heavy Metal
+Thrash Metal
+Death Metal
+Black Metal
+Doom Metal
+Power Metal
+Symphonic Metal
+Progressive Metal
+Nu Metal
+Alternative Metal
+Folk Metal
+Viking Metal
+Glam Metal
+Metal Ballads
+Power Pop
+Synthpop
+Electropop
+Hyperpop
+Bedroom Pop
+Chamber Pop
+Baroque Pop
+Art Pop
+Dreamy Pop
+Teen Pop
+Dance Pop
+Indie Folk
+Folk Rock
+Singer-Songwriter
+Americana
+Bluegrass
+Contemporary Country
+Alt-Country
+Country Pop
+Country Rock
+Honky-Tonk
+Old-Time Music
+Delta Blues
+Chicago Blues
+Electric Blues
+Blues Rock
+Soul Blues
+Traditional R&B
+Contemporary R&B
+Alternative R&B
+Neo Soul
+Motown
+Funk
+P-Funk
+Disco
+Boogie
+Quiet Storm
+New Jack Swing
+Hip-Hop
+Old-School Hip-Hop
+Golden Age Hip-Hop
+East Coast Hip-Hop
+West Coast Hip-Hop
+Southern Hip-Hop
+Trap
+Drill
+Boom Bap
+Conscious Hip-Hop
+Alternative Hip-Hop
+Jazz Rap
+Lo-Fi Hip-Hop
+Cloud Rap
+Grime
+UK Garage
+Afrobeats
+Amapiano
+Reggaeton
+Latin Pop
+Latin Trap
+Salsa
+Bachata
+Merengue
+Bossa Nova
+Samba
+MPB
+Tango
+Flamenco
+Reggae
+Roots Reggae
+Dub
+Dancehall
+Ska
+Rocksteady
+Jazz
+Bebop
+Hard Bop
+Cool Jazz
+Modal Jazz
+Free Jazz
+Jazz Fusion
+Smooth Jazz
+Latin Jazz
+Big Band
+Swing Jazz
+Vocal Jazz
+Contemporary Jazz
+Acid Jazz
+Classical Crossover
+Baroque Music
+Classical Period
+Romantic Classical
+Modern Classical
+Contemporary Classical
+Minimalism
+Opera
+Choral Music
+Chamber Music
+Orchestral Music
+Piano Classical
+Violin Classical
+Film Scores
+Video Game Music|vgm
+Anime Soundtracks
+Musical Theatre Songs
+Ambient
+Dark Ambient
+Drone Music
+New Age
+Downtempo
+Trip-Hop
+Chillout
+Lounge Music
+IDM
+Breakbeat
+Jungle
+Liquid Drum & Bass
+Neurofunk
+Dubstep
+Future Bass
+Trap EDM
+Electro House
+Progressive House
+Deep House
+Tech House
+French House
+Disco House
+Acid House
+Chicago House
+Detroit Techno
+Minimal Techno
+Hard Techno
+Melodic Techno
+Progressive Trance
+Psytrance
+Goa Trance
+Hardstyle
+Gabber
+UK Hardcore
+Synthwave
+Vaporwave
+Chillwave
+Future Funk
+Electro Swing
+K-Pop Girl Groups
+K-Pop Boy Groups
+Korean Indie
+Korean R&B
+Korean Hip-Hop
+Korean Ballads
+J-Rock
+Japanese Indie
+Japanese City Pop
+Japanese Jazz
+Japanese Hip-Hop
+Japanese Alternative
+Vocaloid
+Anisong
+Visual Kei
+Shibuya-kei
+Cantopop Classics
+Modern Cantopop
+Hong Kong Indie
+Hong Kong Rock
+Mandopop Ballads
+Mandopop Rock
+Taiwan Indie
+Chinese Rock
+Chinese Hip-Hop
+Chinese Folk
+C-Pop Idol Music
+World Music
+Celtic Music
+African Music
+Highlife
+Fela Kuti / Afrobeat
+Middle Eastern Music
+Indian Classical Music
+Hindustani Classical
+Carnatic Classical
+Bollywood Music
+Qawwali
+Ghazal
+Japanese Traditional Music
+Chinese Traditional Music
+Chinese Opera Music
+Cantonese Opera
+Guzheng Music
+Erhu Music
+Taiko
+Korean Traditional Music
+Lo-Fi Beats
+Study Music
+Workout Music
+Sleep Music
+Meditation Music
+Road Trip Music
+Acoustic Covers
+Live Albums
+Unplugged Music
+Vinyl Listening
+Audiophile Music
+''',
+  ),
+  ...parseInterestFamily(
+    idPrefix: 'music.global_artist',
+    category: 'music',
+    cluster: 'music/artists_global',
+    rankStart: 3400,
+    raw: r'''
+The Beatles
+The Rolling Stones
+Led Zeppelin
+Pink Floyd
+Queen
+David Bowie
+Elton John
+Fleetwood Mac
+The Beach Boys
+The Doors
+The Who
+The Kinks
+Creedence Clearwater Revival
+Eagles
+Aerosmith
+AC/DC
+Black Sabbath
+Deep Purple
+Rush
+Genesis
+Yes
+King Crimson
+Dire Straits
+U2
+R.E.M.
+The Cure
+Depeche Mode
+New Order
+Joy Division
+The Smiths
+Talking Heads
+The Police
+Duran Duran
+Pet Shop Boys
+Tears for Fears
+Nirvana
+Pearl Jam
+Soundgarden
+Alice in Chains
+Red Hot Chili Peppers
+Foo Fighters
+Radiohead
+Oasis
+Blur
+Pulp
+Suede
+The Stone Roses
+Arctic Monkeys
+Muse
+Coldplay
+The Killers
+The Strokes
+The White Stripes
+Franz Ferdinand
+Tame Impala
+Vampire Weekend
+The National
+Bon Iver
+Arcade Fire
+Florence + the Machine
+Paramore
+Fall Out Boy
+My Chemical Romance
+Green Day
+Blink-182
+Linkin Park
+System of a Down
+Slipknot
+Metallica
+Iron Maiden
+Judas Priest
+Megadeth
+Slayer
+Tool
+Nine Inch Nails
+Rammstein
+Evanescence
+Bring Me the Horizon
+Taylor Swift
+Beyoncé
+Rihanna
+Lady Gaga
+Adele
+Ariana Grande
+Billie Eilish
+Dua Lipa
+Olivia Rodrigo
+Sabrina Carpenter
+Lana Del Rey
+Lorde
+Miley Cyrus
+Katy Perry
+Britney Spears
+Christina Aguilera
+Madonna
+Whitney Houston
+Mariah Carey
+Celine Dion
+Janet Jackson
+Michael Jackson
+Prince
+Stevie Wonder
+Aretha Franklin
+Marvin Gaye
+Diana Ross
+Donna Summer
+Earth, Wind & Fire
+Bee Gees
+ABBA
+Bruno Mars
+The Weeknd
+Frank Ocean
+SZA
+Daniel Caesar
+H.E.R.
+Alicia Keys
+Usher
+Justin Timberlake
+Justin Bieber
+Ed Sheeran
+Harry Styles
+One Direction
+Backstreet Boys
+Spice Girls
+NSYNC
+Doja Cat
+Tyler, the Creator
+Kendrick Lamar
+Drake
+J. Cole
+Kanye West
+Jay-Z
+Nas
+Eminem
+2Pac
+The Notorious B.I.G.
+Snoop Dogg
+Dr. Dre
+Wu-Tang Clan
+OutKast
+A Tribe Called Quest
+Public Enemy
+Lauryn Hill
+Missy Elliott
+Nicki Minaj
+Cardi B
+Travis Scott
+Future
+Post Malone
+Mac Miller
+Childish Gambino
+Anderson .Paak
+Daft Punk
+The Chemical Brothers
+The Prodigy
+Massive Attack
+Portishead
+Aphex Twin
+Boards of Canada
+Kraftwerk
+Moby
+Fatboy Slim
+Justice
+Calvin Harris
+Avicii
+Deadmau5
+Skrillex
+Disclosure
+Fred again..
+Charli xcx
+Chappell Roan
+Phoebe Bridgers
+Boygenius
+Mitski
+Clairo
+Beabadoobee
+Laufey
+Hozier
+Noah Kahan
+Zach Bryan
+Chris Stapleton
+Dolly Parton
+Johnny Cash
+Willie Nelson
+Shania Twain
+Kacey Musgraves
+Bob Dylan
+Joni Mitchell
+Neil Young
+Simon & Garfunkel
+Leonard Cohen
+Carole King
+James Taylor
+Tracy Chapman
+Norah Jones
+Amy Winehouse
+Nina Simone
+Ella Fitzgerald
+Billie Holiday
+Louis Armstrong
+Miles Davis
+John Coltrane
+Chet Baker
+Herbie Hancock
+Chick Corea
+Pat Metheny
+Duke Ellington
+Count Basie
+Dave Brubeck
+Oscar Peterson
+B.B. King
+Muddy Waters
+Eric Clapton
+Carlos Santana
+Bob Marley
+Buju Banton
+Sean Paul
+Bad Bunny
+J Balvin
+Karol G
+Shakira
+Rosalía
+Daddy Yankee
+Luis Miguel
+Juanes
+Caetano Veloso
+João Gilberto
+Antonio Carlos Jobim
+Fela Kuti
+Burna Boy
+Wizkid
+Tems
+Stromae
+Christine and the Queens
+Phoenix
+Air
+Måneskin
+Andrea Bocelli
+Ennio Morricone
+Hans Zimmer
+John Williams
+Joe Hisaishi|久石讓|久石譲|久石让
+Ryuichi Sakamoto|坂本龍一|坂本龍一|坂本龙一
+Yiruma
+Ludovico Einaudi
+Max Richter
+Ólafur Arnalds
+Nils Frahm
+''',
+  ),
+  ...parseInterestFamily(
+    idPrefix: 'music.kpop_artist',
+    category: 'music',
+    cluster: 'music/kpop_artists',
+    rankStart: 3800,
+    raw: r'''
+BTS
+BLACKPINK
+TWICE
+EXO
+SEVENTEEN
+Stray Kids
+NewJeans
+IVE
+aespa
+LE SSERAFIM
+(G)I-DLE
+Red Velvet
+Girls' Generation|SNSD
+SHINee
+BIGBANG
+2NE1
+Super Junior
+TVXQ
+Wonder Girls
+KARA
+2PM
+f(x)
+Apink
+MAMAMOO
+GOT7
+MONSTA X
+NCT
+NCT 127
+NCT Dream
+WayV
+ATEEZ
+TXT
+ENHYPEN
+TREASURE
+ZEROBASEONE
+RIIZE
+BOYNEXTDOOR
+TWS
+ILLIT
+BABYMONSTER
+ITZY
+NMIXX
+STAYC
+KISS OF LIFE
+Dreamcatcher
+LOONA
+tripleS
+fromis_9
+OH MY GIRL
+GFRIEND
+IU
+Taeyeon
+BoA
+Sunmi
+HyunA
+Chungha
+Heize
+BIBI
+Dean
+Crush
+Zion.T
+Epik High
+Dynamic Duo
+Zico
+Jay Park
+DPR IAN
+DPR LIVE
+LeeHi
+AKMU
+DAY6
+The Rose
+Xdinary Heroes
+CNBLUE
+FTIsland
+Jannabi
+Nell
+HYUKOH
+NewJeans Members
+BTS Solo Projects
+BLACKPINK Solo Projects
+''',
+  ),
+  ...parseInterestFamily(
+    idPrefix: 'music.jp_artist',
+    category: 'music',
+    cluster: 'music/japanese_artists',
+    rankStart: 3950,
+    raw: r'''
+Utada Hikaru|宇多田光
+Ayumi Hamasaki|濱崎步|浜崎あゆみ|滨崎步
+Namie Amuro|安室奈美惠
+Kenshi Yonezu|米津玄師|米津玄師|米津玄师
+YOASOBI
+Ado
+LiSA
+Aimyon|愛繆|あいみょん|爱缪
+Mrs. GREEN APPLE
+Official HIGE DANdism|Official髭男dism
+King Gnu
+back number
+RADWIMPS
+BUMP OF CHICKEN
+ONE OK ROCK
+MAN WITH A MISSION
+Asian Kung-Fu Generation
+L'Arc-en-Ciel
+GLAY
+X Japan
+Luna Sea
+B'z
+Mr.Children
+Southern All Stars
+Spitz
+The Blue Hearts
+Number Girl
+Fishmans
+Cornelius
+Pizzicato Five
+Perfume
+Kyary Pamyu Pamyu
+Sakanaction
+Supercar
+YMO|Yellow Magic Orchestra
+Mariya Takeuchi|竹內瑪莉亞|竹内まりや|竹内玛利亚
+Tatsuro Yamashita|山下達郎|山下達郎|山下达郎
+Anri|杏里
+Miki Matsubara|松原美紀|松原みき|松原美纪
+Taeko Ohnuki|大貫妙子
+Haruomi Hosono|細野晴臣
+Eve
+Vaundy
+Fujii Kaze|藤井風|藤井風|藤井风
+Creepy Nuts
+Atarashii Gakko!|新しい学校のリーダーズ
+Babymetal
+Band-Maid
+SCANDAL
+Maximum the Hormone
+Ling Tosite Sigure
+TK from Ling Tosite Sigure
+Aimer
+milet
+ReoNa
+ClariS
+Kalafina
+Yuki Kajiura|梶浦由記
+Hiroyuki Sawano|澤野弘之|澤野弘之|泽野弘之
+Linked Horizon
+FLOW
+Porno Graffitti
+Orange Range
+KANA-BOON
+Ikimonogakari
+ZUTOMAYO|ずっと真夜中でいいのに。
+yorushika|ヨルシカ
+Mafumafu|まふまふ
+Hatsune Miku|初音未來|初音ミク|初音未来
+Vocaloid Producers
+''',
+  ),
+  ...parseInterestFamily(
+    idPrefix: 'music.hk_artist',
+    category: 'music',
+    cluster: 'music/hk_cantopop',
+    rankStart: 4100,
+    raw: r'''
+Leslie Cheung|張國榮|張國榮|张国荣
+Anita Mui|梅艷芳|梅艷芳|梅艳芳
+Beyond
+Danny Chan|陳百強|陳百強|陈百强
+Alan Tam|譚詠麟|譚詠麟|谭咏麟
+Roman Tam|羅文|羅文|罗文
+George Lam|林子祥|林子祥
+Paula Tsui|徐小鳳|徐小鳳|徐小凤
+Sam Hui|許冠傑|許冠傑|许冠杰
+Priscilla Chan|陳慧嫻|陳慧嫻|陈慧娴
+Sandy Lam|林憶蓮|林憶蓮|林忆莲
+Faye Wong|王菲
+Jacky Cheung|張學友|張學友|张学友
+Andy Lau|劉德華|劉德華|刘德华
+Leon Lai|黎明
+Aaron Kwok|郭富城
+Eason Chan|陳奕迅|陳奕迅|陈奕迅
+Joey Yung|容祖兒|容祖兒|容祖儿
+Hacken Lee|李克勤
+Kelly Chen|陳慧琳|陳慧琳|陈慧琳
+Miriam Yeung|楊千嬅|楊千嬅|杨千嬅
+Sammi Cheng|鄭秀文|鄭秀文|郑秀文
+Nicholas Tse|謝霆鋒|謝霆鋒|谢霆锋
+Leo Ku|古巨基
+Edmond Leung|梁漢文|梁漢文|梁汉文
+Janice Vidal|衛蘭|衛蘭|卫兰
+Ivana Wong|王菀之
+Kay Tse|謝安琪|謝安琪|谢安琪
+Hins Cheung|張敬軒|張敬軒|张敬轩
+Pakho Chau|周柏豪
+Dear Jane
+RubberBand
+Supper Moment
+C AllStar
+My Little Airport
+The Pancakes
+Chochukmo
+GDJYB|雞蛋蒸肉餅
+ToNick
+Kolor
+Nowhere Boys
+Mirror
+Anson Lo|盧瀚霆|卢瀚霆
+Keung To|姜濤|姜涛
+Edan Lui|呂爵安|吕爵安
+Jer Lau|柳應廷|柳应廷
+Ian Chan|陳卓賢|陈卓贤
+MC Cheung Tinfu|張天賦|张天赋
+Terence Lam|林家謙|林家谦
+Jay Fung|馮允謙|冯允谦
+Kaho Hung|洪嘉豪
+Cloud Wan|雲浩影|云浩影
+Jace Chan|陳凱詠|陈凯咏
+Serrini
+moon tang
+Gareth.T
+Tyson Yoshi
+Novel Fergus
+JB
+Lolly Talk
+COLLAR
+''',
+  ),
+  ...parseInterestFamily(
+    idPrefix: 'music.mando_artist',
+    category: 'music',
+    cluster: 'music/mandopop_artists',
+    rankStart: 4250,
+    raw: r'''
+Jay Chou|周杰倫|周杰伦
+JJ Lin|林俊傑|林俊杰
+Mayday|五月天
+Sodagreen|蘇打綠|苏打绿
+Stefanie Sun|孫燕姿|孙燕姿
+Jolin Tsai|蔡依林
+A-Mei|張惠妹|张惠妹
+David Tao|陶喆
+Leehom Wang|王力宏
+Fish Leong|梁靜茹|梁静茹
+Rainie Yang|楊丞琳|杨丞琳
+Hebe Tien|田馥甄
+S.H.E
+F.I.R.
+Cheer Chen|陳綺貞|陈绮贞
+Deserts Chang|張懸|张悬
+Crowd Lu|盧廣仲|卢广仲
+Wu Bai|伍佰
+Lo Ta-yu|羅大佑|罗大佑
+Jonathan Lee|李宗盛
+Tayu Lo|羅大佑|罗大佑
+Teresa Teng|鄧麗君|邓丽君
+Fei Yu-ching|費玉清|费玉清
+Wakin Chau|周華健|周华健
+Richie Jen|任賢齊|任贤齐
+Jeff Chang|張信哲|张信哲
+Eric Moo|巫啟賢|巫启贤
+Karen Mok|莫文蔚
+Na Ying|那英
+Faye Wong Mandarin|王菲國語歌|王菲国语歌
+Eason Chan Mandarin|陳奕迅國語歌|陈奕迅国语歌
+Li Ronghao|李榮浩|李荣浩
+Hua Chenyu|華晨宇|华晨宇
+Zhou Shen|周深
+Mao Buyi|毛不易
+Joker Xue|薛之謙|薛之谦
+G.E.M.|鄧紫棋|邓紫棋
+Accusefive|告五人
+No Party For Cao Dong|草東沒有派對|草东没有派对
+Sunset Rollercoaster|落日飛車|落日飞车
+Elephant Gym|大象體操
+The Chairs|椅子樂團|椅子乐团
+Cosmos People|宇宙人
+EggPlantEgg|茄子蛋
+Fire EX.|滅火器|灭火器
+Omnipotent Youth Society|萬能青年旅店|万能青年旅店
+New Pants|新褲子|新裤子
+Miserable Faith|痛仰
+Second Hand Rose|二手玫瑰
+Carsick Cars
+The Big Wave|大波浪
+Hedgehog|刺猬
+Faye Wong Classics|王菲經典|王菲经典
+''',
+  ),
+]);
