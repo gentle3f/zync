edc1955fd1c1812c753b2c37bb7e45fa778b7b34
gentle3f <gentle3f@gmail.com>
2026-09-18 02:13:02 +0800

    Add deep gaming and tabletop interest families

---
+++ mobile/lib/core/interest_catalog_part8.dart
@@ -0,0 +1,710 @@
+import 'interest_catalog_parser.dart';
+import 'models.dart';
+
+final List<InterestDefinition> kInterestCatalogPart8 = List.unmodifiable([
+  ...parseInterestFamily(
+    idPrefix: 'gaming.subgenre',
+    category: 'gaming',
+    cluster: 'gaming/subgenres',
+    rankStart: 2100,
+    raw: r'''
+Action RPG
+Action Roguelike
+Action RTS
+Action-Adventure Games
+Arena Shooter
+Auto Battler
+Automation Games
+Base Building Games
+Battle Royale
+Beat 'em Up
+Boomer Shooter
+Bullet Hell
+Bullet Heaven
+Card Battler
+Character Action Games
+City Builder
+Co-op Campaign Games
+Colony Sim
+Combat Racing
+Creature Collector
+CRPG
+Deckbuilding Games
+Detective Games
+Dungeon Crawler
+Extraction Shooter
+Farming Sim
+First-Person Adventure
+Grand Strategy
+Hack and Slash
+Hero Shooter
+Hidden Object Games
+Immersive Sim
+Interactive Fiction
+Life Sim
+Looter Shooter
+Management Games
+Match-3 Games
+Metroidvania
+Military Games
+MMORPG
+MOBA
+Mystery Dungeon
+Open World Games
+Open World Survival Craft
+Party Games
+Party-Based RPG
+Platformers
+Point-and-Click Adventure
+Precision Platformer
+Programming Games
+Puzzle Platformer
+Real-Time Tactics
+Rhythm Action Games
+Roguelite
+Roguelike Deckbuilder
+RTS
+Sandbox Games
+Shoot 'Em Up
+Social Deduction Games
+Soulslike
+Space Sim
+Stealth Games
+Strategy RPG
+Survival Horror
+Tactical RPG
+Third-Person Shooter
+Tower Defense
+Traditional Roguelike
+Turn-Based Combat
+Turn-Based Strategy
+Turn-Based Tactics
+Twin-Stick Shooter
+Walking Simulator
+4X Strategy
+Cozy Games
+Story-Rich Games
+Choices-Matter Games
+Multiple-Ending Games
+Lore-Rich Games
+Narrative Games
+Exploration Games
+Crafting Games
+Resource Management Games
+Economy Games
+Political Sim
+God Games
+Job Simulators
+Shopkeeper Games
+Medical Sim
+Train Sim
+Flight Sim
+Automobile Sim
+Truck Sim
+Hunting Games
+Fishing Games
+Skateboarding Games
+Snowboarding Games
+Skiing Games
+Golf Games
+Tennis Games
+Boxing Games
+Wrestling Games
+Football Games
+Basketball Games
+Baseball Games
+Motorsport Games
+Naval Games
+Tank Games
+Mech Games
+Space Combat Games
+Pirate Games
+Ninja Games
+Samurai Games
+Martial Arts Games
+Cyberpunk Games
+Post-Apocalyptic Games
+Lovecraftian Games
+Vampire Games
+Zombie Games
+Dinosaur Games
+Mythology Games
+Historical Games
+Alternate-History Games
+World War II Games
+Cold War Games
+Medieval Games
+Western Games
+Superhero Games
+Anime Games
+Otome Games
+Dating Sims
+Visual Novel Mystery
+Escape-Room Games
+Trivia Games
+Word Games
+Typing Games
+Chess Games
+Mahjong Games
+Poker Games
+Pinball Games
+Incremental Games
+Idle Games
+Collectathons
+Speedrunning
+Modding Games
+Level Editors
+Game Creation Tools
+''',
+  ),
+  ...parseInterestFamily(
+    idPrefix: 'gaming.franchise',
+    category: 'gaming',
+    cluster: 'gaming/franchises',
+    rankStart: 2300,
+    raw: r'''
+Baldur's Gate
+The Witcher
+Cyberpunk 2077
+Mass Effect
+Dragon Age
+The Elder Scrolls
+Skyrim
+Fallout
+Starfield
+Diablo
+World of Warcraft
+Warcraft
+StarCraft
+Overwatch
+Hearthstone
+Destiny
+Halo
+Gears of War
+Forza Horizon
+Forza Motorsport
+Age of Empires
+Microsoft Flight Simulator
+Sea of Thieves
+Fable
+DOOM
+Quake
+Wolfenstein
+Dishonored
+Prey
+The Last of Us
+Uncharted
+God of War
+Horizon
+Ghost of Tsushima
+Bloodborne
+Demon's Souls
+Ratchet & Clank
+Gran Turismo
+Shadow of the Colossus
+The Last Guardian
+LittleBigPlanet
+Astro Bot
+Persona
+Shin Megami Tensei
+Yakuza / Like a Dragon|yakuza;ryu ga gotoku
+Kingdom Hearts
+NieR
+Dragon Quest
+Chrono Trigger
+Xenoblade Chronicles
+Fire Emblem
+Metroid
+Kirby
+Donkey Kong
+Super Smash Bros.
+Pikmin
+Bayonetta
+Xenogears
+Octopath Traveler
+Bravely Default
+Tales Series
+Ys Series
+Trails / Kiseki
+Suikoden
+Mana Series
+SaGa Series
+Ace Attorney
+Professor Layton
+Phoenix Wright
+Danganronpa
+Zero Escape
+AI: The Somnium Files
+13 Sentinels: Aegis Rim
+Metal Gear Solid
+Death Stranding
+Silent Hill
+Resident Evil
+Devil May Cry
+Street Fighter
+Tekken
+Mortal Kombat
+Soulcalibur
+Guilty Gear
+BlazBlue
+King of Fighters
+Fatal Fury
+Monster Rancher
+Digimon Games
+Yo-kai Watch
+Rune Factory
+Story of Seasons
+Harvest Moon
+Stardew Valley
+Terraria
+Don't Starve
+Subnautica
+ARK: Survival Evolved
+Rust
+DayZ
+Valheim
+Palworld
+No Man's Sky
+Outer Wilds
+The Outer Worlds
+Portal
+Half-Life
+Left 4 Dead
+Team Fortress 2
+Garry's Mod
+Apex Legends
+PUBG
+Call of Duty
+Battlefield
+Rainbow Six Siege
+Escape from Tarkov
+Warframe
+Helldivers
+Borderlands
+BioShock
+Far Cry
+Assassin's Creed
+Prince of Persia
+Watch Dogs
+Tom Clancy's Splinter Cell
+Hitman
+Tomb Raider
+Just Cause
+Saints Row
+Red Dead Redemption
+Max Payne
+Mafia
+Sleeping Dogs
+L.A. Noire
+Deathloop
+Control
+Alan Wake
+Quantum Break
+Remedy Connected Universe
+Hades
+Hollow Knight
+Celeste
+Dead Cells
+Cuphead
+Ori
+Undertale
+Deltarune
+Disco Elysium
+Inscryption
+Slay the Spire
+Balatro
+Vampire Survivors
+Enter the Gungeon
+The Binding of Isaac
+Risk of Rain
+Spelunky
+FTL: Faster Than Light
+Into the Breach
+Factorio
+Satisfactory
+Dyson Sphere Program
+RimWorld
+Dwarf Fortress
+Oxygen Not Included
+Kerbal Space Program
+Cities: Skylines
+SimCity
+RollerCoaster Tycoon
+Planet Coaster
+Two Point Hospital
+Theme Hospital
+Zoo Tycoon
+Planet Zoo
+Anno
+Crusader Kings
+Europa Universalis
+Hearts of Iron
+Stellaris
+Total War
+XCOM
+Command & Conquer
+Company of Heroes
+Homeworld
+Warcraft III
+Heroes of Might and Magic
+Civilization VI
+Sid Meier's Alpha Centauri
+Football Manager Series
+EA Sports FC|fifa games
+NBA 2K
+Madden NFL
+MLB The Show
+WWE 2K
+Rocket League
+Trackmania
+Need for Speed
+Burnout
+Mario Kart
+F-Zero
+Wipeout
+Assetto Corsa
+iRacing
+Gran Turismo 7
+Forza Motorsport Series
+Dirt Rally
+F1 Games
+Microsoft Train Simulator
+Euro Truck Simulator
+American Truck Simulator
+The Sims 4
+Animal Well
+Dave the Diver
+Dredge
+Cult of the Lamb
+Spiritfarer
+A Short Hike
+Unpacking
+PowerWash Simulator
+House Flipper
+Cooking Mama
+Overcooked
+Moving Out
+Human: Fall Flat
+Gang Beasts
+Fall Guys
+Among Us
+Jackbox Party Pack
+Keep Talking and Nobody Explodes
+Phasmophobia
+Lethal Company
+Dead by Daylight
+Five Nights at Freddy's
+Amnesia
+Outlast
+SOMA
+Layers of Fear
+Little Nightmares
+Until Dawn
+The Quarry
+Life is Strange
+Detroit: Become Human
+Heavy Rain
+The Walking Dead by Telltale
+Telltale Games
+Minecraft Dungeons
+Minecraft Legends
+Roblox Experiences
+Fortnite Creative
+Genshin Impact Characters
+Honkai Impact 3rd
+Zenless Zone Zero
+Wuthering Waves
+Arknights
+Azur Lane
+Fate/Grand Order
+Blue Archive
+Nikke
+Uma Musume
+Granblue Fantasy
+Final Fantasy XIV|ffxiv
+Final Fantasy VII
+Final Fantasy X
+Final Fantasy XVI
+World of Final Fantasy
+Pokémon Scarlet and Violet
+Pokémon Legends
+Pokémon Go
+Pokémon Unite
+Pokémon Mystery Dungeon
+The Legend of Zelda: Breath of the Wild|botw
+The Legend of Zelda: Tears of the Kingdom|totk
+Ocarina of Time
+Majora's Mask
+Super Mario Odyssey
+Super Mario Galaxy
+Super Mario Bros. Wonder
+Paper Mario
+Mario Party
+Luigi's Mansion
+Splatoon 3
+Animal Crossing: New Horizons
+''',
+  ),
+  ...parseInterestFamily(
+    idPrefix: 'gaming.tabletop_style',
+    category: 'gaming',
+    cluster: 'tabletop/categories_mechanics',
+    rankStart: 2700,
+    raw: r'''
+Abstract Strategy Games
+Area Control Games
+Auction Games
+Bluffing Games
+Campaign Board Games
+Card Drafting Games
+City-Building Board Games
+Civilization Board Games
+Cooperative Board Games
+Deck-Building Board Games
+Deduction Games
+Dexterity Games
+Dice Games
+Economic Board Games
+Engine-Building Games
+Eurogames
+Hidden-Traitor Games
+Legacy Board Games
+Miniature Wargames
+Negotiation Games
+Party Board Games
+Push-Your-Luck Games
+Racing Board Games
+Roll-and-Write Games
+Set Collection Games
+Social Deduction Board Games
+Storytelling Board Games
+Tile-Laying Games
+Trading Games
+Train Board Games
+Worker Placement Games
+Wargames
+Word Board Games
+Escape-Room Board Games
+Living Card Games
+Collectible Card Games
+Trading Card Games
+Role-Playing Board Games
+Dungeon-Crawl Board Games
+Adventure Board Games
+Horror Board Games
+Science-Fiction Board Games
+Fantasy Board Games
+Historical Board Games
+Nature Board Games
+Cozy Board Games
+Two-Player Board Games
+Solo Board Games
+Family Board Games
+Heavy Strategy Board Games
+Light Strategy Board Games
+Print-and-Play Games
+Board Game Design
+Board Game Cafes
+Board Game Conventions
+Miniature Painting
+Terrain Building
+Role-Playing Miniatures
+Dice Collecting
+Card Sleeving
+Board Game Organizing
+''',
+  ),
+  ...parseInterestFamily(
+    idPrefix: 'gaming.boardgame_title',
+    category: 'gaming',
+    cluster: 'tabletop/boardgame_titles',
+    rankStart: 2800,
+    raw: r'''
+Pandemic
+Pandemic Legacy
+Wingspan
+Terraforming Mars
+Brass: Birmingham
+Brass: Lancashire
+Ark Nova
+Dune: Imperium
+Dune: Imperium Uprising
+Spirit Island
+Scythe
+Root
+Oath
+Pax Pamir
+Twilight Struggle
+Twilight Imperium
+Eclipse
+Gaia Project
+Terra Mystica
+Agricola
+Caverna
+A Feast for Odin
+Viticulture
+Concordia
+Puerto Rico
+Power Grid
+Castles of Burgundy
+Grand Austria Hotel
+Great Western Trail
+Arkham Horror
+Eldritch Horror
+Mansions of Madness
+Marvel Champions
+The Lord of the Rings: The Card Game
+Netrunner
+Dominion
+Ascension
+Star Realms
+Hero Realms
+Race for the Galaxy
+7 Wonders
+7 Wonders Duel
+Splendor
+Azul
+Sagrada
+Patchwork
+Codenames
+Decrypto
+Just One
+Dixit
+Wavelength
+The Resistance
+Avalon
+Secret Hitler
+Blood on the Clocktower
+Werewolf
+Coup
+Love Letter
+Exploding Kittens
+Sushi Go!
+King of Tokyo
+Small World
+Stone Age
+Lords of Waterdeep
+Betrayal at House on the Hill
+Dead of Winter
+Nemesis
+Zombicide
+Mage Knight
+Descent
+HeroQuest
+Mice and Mystics
+Clank!
+Clank! Catacombs
+The Crew
+The Mind
+Hanabi
+Skull
+Santorini
+Onitama
+Hive
+Quoridor
+Blokus
+Go Board Game
+Shogi
+Xiangqi Board Game
+Backgammon
+Checkers
+Carrom
+Crokinole
+Monopoly
+Risk
+Cluedo / Clue
+Scrabble
+Boggle
+Pictionary
+Trivial Pursuit
+Uno
+Rummikub
+Sequence
+Ticket to Ride Europe
+Carcassonne: Hunters and Gatherers
+Catan: Cities & Knights
+Catan: Seafarers
+Munchkin
+BANG!
+Citadels
+Power Plants
+Heat: Pedal to the Metal
+Flamme Rouge
+Formula D
+Memoir '44
+Commands & Colors
+War of the Ring
+Star Wars: Rebellion
+Star Wars: Imperial Assault
+Disney Villainous
+Horrified
+Everdell
+Cascadia
+Calico
+Parks
+Meadow
+Forest Shuffle
+Earth
+Photosynthesis
+Takenoko
+Tokaido
+Jaipur
+Lost Cities
+Watergate
+Radlands
+Air, Land & Sea
+''',
+  ),
+  ...parseInterestFamily(
+    idPrefix: 'gaming.ttrpg',
+    category: 'gaming',
+    cluster: 'tabletop/ttrpg',
+    rankStart: 3000,
+    raw: r'''
+Pathfinder
+Pathfinder 2e
+Call of Cthulhu RPG
+Cyberpunk RED
+Shadowrun
+Vampire: The Masquerade
+Werewolf: The Apocalypse
+World of Darkness
+Warhammer Fantasy Roleplay
+Warhammer 40K Roleplay
+Star Wars Roleplaying Games
+Blades in the Dark
+Fate Core
+Powered by the Apocalypse
+Monster of the Week
+Masks: A New Generation
+Dungeon World
+Kids on Bikes
+Mörk Borg
+Cairn
+Old-School Essentials
+OSR Roleplaying
+Traveller RPG
+Delta Green
+Lancer RPG
+Numenera
+Genesys RPG
+Legend of the Five Rings RPG
+The One Ring RPG
+Alien RPG
+Vaesen
+Tales from the Loop RPG
+Mouse Guard RPG
+Fabula Ultima
+Final Fantasy XIV TTRPG
+Critical Role
+Dimension 20
+Actual Play RPG Shows
+Dungeon Mastering
+Game Mastering
+RPG Worldbuilding
+RPG Miniatures
+RPG Dice
+RPG Map Making
+''',
+  ),
+]);
