4b0b7fc2639b97b74aa89a5563759ed10e4ecc7a
gentle3f <gentle3f@gmail.com>
2026-09-19 21:09:13 +0800

    test: keep standalone Zync Now visible on Home

--- mobile/test/layout_smoke_test.dart
+++ mobile/test/layout_smoke_test.dart
@@ -41,6 +41,7 @@ void main() {
     expect(find.text('Show my QR'), findsOneWidget);
     expect(find.text('Scan someone'), findsOneWidget);
     expect(find.text('Group Zync'), findsOneWidget);
+    expect(find.text('Zync Now'), findsOneWidget);
     expect(find.byType(ListView), findsNothing);
     expect(tester.takeException(), isNull);
   });
