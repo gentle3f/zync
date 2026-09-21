9e05a1b979c84df3bb952106531321a3417bac7e
gentle3f <gentle3f@gmail.com>
2026-09-22 00:24:21 +0800

    fix: distinguish canoeing from kayaking canonical labels

--- mobile/lib/core/interest_catalog_part1.dart
+++ mobile/lib/core/interest_catalog_part1.dart
@@ -91,7 +91,7 @@ outdoors.scuba_diving|outdoors|water|177|Scuba Diving|水肺潛水|水肺潜水|
 outdoors.freediving|outdoors|water|178|Freediving|自由潛水|自由潜水|自由潛水;自由潜水
 outdoors.snorkeling|outdoors|water|179|Snorkeling|浮潛|浮潜|浮潛;浮潜
 outdoors.kayaking|outdoors|water|180|Kayaking|獨木舟|皮划艇|獨木舟;皮划艇
-outdoors.canoeing|outdoors|water|181|Canoeing|獨木舟|皮划艇|划艇
+outdoors.canoeing|outdoors|water|181|Canoeing|加拿大式獨木舟|加拿大式划艇|canoe
 outdoors.sailing|outdoors|water|182|Sailing|帆船|帆船|帆船
 outdoors.stand_up_paddleboarding|outdoors|water|183|Stand-up Paddleboarding|直立板|桨板|sup
 outdoors.windsurfing|outdoors|water|184|Windsurfing|滑浪風帆|帆板|
